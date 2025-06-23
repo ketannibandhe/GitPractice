e783d95c76caae70d9740b3281083ed56a4a0934
Ketan Nibandhe <nibandheketan2354@gmail.com>
2025-06-23 07:17:56 +0530

    BigFix branch changed

--- first.js
+++ first.js
@@ -4,3 +4,10 @@ console.log("5");
 
 console.log("10000");
 console.log("I want to see whether the changes made here, reflects in the github repo or not");
+
+console.log("You are now in Bigfix branch");
+let nums = [1, 2, 3, 4, 5];
+let doubled = nums.map(n => n * 2);
+console.log("Doubled Array:", doubled);
+document.body.style.backgroundColor = "#f0f8ff";
+alert("JavaScript is running!");
